669662ce96bdcd3cd931ca378faed2424b81db03
thanhBriswell <thanh.clg@briswell.com>
2018-10-19 10:47:04 +0700

    ###added 興行区分ルーターを追加

--- dst/app/routes/boxOfficeTypes.js
+++ dst/app/routes/boxOfficeTypes.js
@@ -13,6 +13,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
  */
 const chevre = require("@toei-jp/chevre-domain");
 const express_1 = require("express");
+const http_status_1 = require("http-status");
 const authentication_1 = require("../middlewares/authentication");
 const permitScopes_1 = require("../middlewares/permitScopes");
 const validator_1 = require("../middlewares/validator");
@@ -25,6 +26,71 @@ boxOfficeTypesRouter.get('/getBoxOfficeTypeList', permitScopes_1.default(['admin
         const boxOfficeTypeRepo = new chevre.repository.BoxOfficeType(chevre.mongoose.connection);
         const boxOfficeTypes = yield boxOfficeTypeRepo.getBoxOfficeTypeList();
         res.json(boxOfficeTypes);
+    }
+    catch (error) {
+        next(error);
+    }
+}));
+boxOfficeTypesRouter.get('/search', permitScopes_1.default(['admin']), (_, __, next) => {
+    next();
+}, validator_1.default, (req, res, next) => __awaiter(this, void 0, void 0, function* () {
+    try {
+        const boxOfficeTypeRepo = new chevre.repository.BoxOfficeType(chevre.mongoose.connection);
+        const searchCondition = {
+            id: req.query.id,
+            name: req.query.name
+        };
+        const totalCount = yield boxOfficeTypeRepo.countBoxOfficeType(searchCondition);
+        const boxOfficeType = yield boxOfficeTypeRepo.searchBoxOfficeType(searchCondition);
+        res.set('X-Total-Count', totalCount.toString());
+        res.json(boxOfficeType);
+    }
+    catch (error) {
+        next(error);
+    }
+}));
+boxOfficeTypesRouter.put('/:id', permitScopes_1.default(['admin']), (req, _, next) => {
+    req.checkBody('name').exists().withMessage('name is required');
+    next();
+}, validator_1.default, (req, res, next) => __awaiter(this, void 0, void 0, function* () {
+    try {
+        const boxOfficeTypeRepo = new chevre.repository.BoxOfficeType(chevre.mongoose.connection);
+        yield boxOfficeTypeRepo.updateBoxOfficeType({
+            id: req.params.id,
+            name: req.body.name
+        });
+        res.status(http_status_1.NO_CONTENT).end();
+    }
+    catch (error) {
+        next(error);
+    }
+}));
+boxOfficeTypesRouter.post('/add', permitScopes_1.default(['admin']), (req, _, next) => {
+    req.checkBody('id').exists().withMessage('id is required');
+    req.checkBody('name').exists().withMessage('name is required');
+    next();
+}, validator_1.default, (req, res, next) => __awaiter(this, void 0, void 0, function* () {
+    try {
+        const boxOfficeTypeRepo = new chevre.repository.BoxOfficeType(chevre.mongoose.connection);
+        const boxOfficeType = yield boxOfficeTypeRepo.createBoxOfficeType({
+            id: req.body.id,
+            name: req.body.name
+        });
+        res.status(http_status_1.CREATED).json(boxOfficeType);
+    }
+    catch (error) {
+        next(error);
+    }
+}));
+boxOfficeTypesRouter.delete('/:id', permitScopes_1.default(['admin']), (_, __, next) => {
+    next();
+}, validator_1.default, (req, res, next) => __awaiter(this, void 0, void 0, function* () {
+    try {
+        const boxOfficeTypeRepo = new chevre.repository.BoxOfficeType(chevre.mongoose.connection);
+        yield boxOfficeTypeRepo.deleteById({
+            id: req.params.id
+        });
+        res.status(http_status_1.NO_CONTENT).end();
     }
     catch (error) {
         next(error);
--- src/app/routes/boxOfficeTypes.ts
+++ src/app/routes/boxOfficeTypes.ts
@@ -4,6 +4,7 @@
 import * as chevre from '@toei-jp/chevre-domain';
 import { Router } from 'express';
 
+import { CREATED, NO_CONTENT } from 'http-status';
 import authentication from '../middlewares/authentication';
 import permitScopes from '../middlewares/permitScopes';
 import validator from '../middlewares/validator';
@@ -22,6 +23,94 @@ boxOfficeTypesRouter.get(
             const boxOfficeTypeRepo = new chevre.repository.BoxOfficeType(chevre.mongoose.connection);
             const boxOfficeTypes = await boxOfficeTypeRepo.getBoxOfficeTypeList();
             res.json(boxOfficeTypes);
+        } catch (error) {
+            next(error);
+        }
+    }
+);
+boxOfficeTypesRouter.get(
+    '/search',
+    permitScopes(['admin']),
+    (_, __, next) => {
+        next();
+    },
+    validator,
+    async (req, res, next) => {
+        try {
+            const boxOfficeTypeRepo = new chevre.repository.BoxOfficeType(chevre.mongoose.connection);
+            const searchCondition = {
+                id: req.query.id,
+                name: req.query.name
+            };
+            const totalCount = await boxOfficeTypeRepo.countBoxOfficeType(searchCondition);
+            const boxOfficeType = await boxOfficeTypeRepo.searchBoxOfficeType(searchCondition);
+            res.set('X-Total-Count', totalCount.toString());
+            res.json(boxOfficeType);
+        } catch (error) {
+            next(error);
+        }
+    }
+);
+
+boxOfficeTypesRouter.put(
+    '/:id',
+    permitScopes(['admin']),
+    (req, _, next) => {
+        req.checkBody('name').exists().withMessage('name is required');
+        next();
+    },
+    validator,
+    async (req, res, next) => {
+        try {
+            const boxOfficeTypeRepo = new chevre.repository.BoxOfficeType(chevre.mongoose.connection);
+            await boxOfficeTypeRepo.updateBoxOfficeType({
+                id: req.params.id,
+                name: req.body.name
+            });
+            res.status(NO_CONTENT).end();
+        } catch (error) {
+            next(error);
+        }
+    }
+);
+
+boxOfficeTypesRouter.post(
+    '/add',
+    permitScopes(['admin']),
+    (req, _, next) => {
+        req.checkBody('id').exists().withMessage('id is required');
+        req.checkBody('name').exists().withMessage('name is required');
+        next();
+    },
+    validator,
+    async (req, res, next) => {
+        try {
+            const boxOfficeTypeRepo = new chevre.repository.BoxOfficeType(chevre.mongoose.connection);
+            const boxOfficeType = await boxOfficeTypeRepo.createBoxOfficeType({
+                id: req.body.id,
+                name: req.body.name
+            });
+            res.status(CREATED).json(boxOfficeType);
+        } catch (error) {
+            next(error);
+        }
+    }
+);
+
+boxOfficeTypesRouter.delete(
+    '/:id',
+    permitScopes(['admin']),
+    (_, __, next) => {
+        next();
+    },
+    validator,
+    async (req, res, next) => {
+        try {
+            const boxOfficeTypeRepo = new chevre.repository.BoxOfficeType(chevre.mongoose.connection);
+            await boxOfficeTypeRepo.deleteById({
+                id: req.params.id
+            });
+            res.status(NO_CONTENT).end();
         } catch (error) {
             next(error);
         }
