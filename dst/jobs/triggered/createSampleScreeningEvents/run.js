1fae16261feb7a199df03191748eaa6e33fd5638
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2018-11-19 06:07:57 +0000

    # CHEVRE-75 ### Changed - 予約の価格属性を価格仕様に変更

--- dst/jobs/triggered/createSampleScreeningEvents/run.js
+++ dst/jobs/triggered/createSampleScreeningEvents/run.js
@@ -20,81 +20,78 @@ const connectMongo_1 = require("../../../connectMongo");
 const debug = createDebug('chevre-api:jobs');
 exports.default = () => __awaiter(this, void 0, void 0, function* () {
     const connection = yield connectMongo_1.connectMongo({ defaultConnection: false });
-    // tslint:disable-next-line:no-increment-decrement
-    for (let i = 0; i < 1; i++) {
-        const job = new cron_1.CronJob('0 * * * *', () => __awaiter(this, void 0, void 0, function* () {
-            const eventRepo = new chevre.repository.Event(connection);
-            const placeRepo = new chevre.repository.Place(connection);
-            const ticketTypeRepo = new chevre.repository.TicketType(connection);
-            const boxOfficeTypeRepo = new chevre.repository.BoxOfficeType(connection);
-            const eventSeriesList = yield eventRepo.searchScreeningEventSeries({});
-            // イベントシリーズをランダム選定
-            const eventSeries = eventSeriesList[Math.floor(Math.random() * eventSeriesList.length)];
-            // 上映ルームをランダム選定
-            const movieTheater = yield placeRepo.findMovieTheaterByBranchCode(eventSeries.location.branchCode);
-            const screeningRooms = movieTheater.containsPlace;
-            const screeningRoom = screeningRooms[Math.floor(Math.random() * screeningRooms.length)];
-            const maximumAttendeeCapacity = screeningRoom.containsPlace.reduce((a, b) => a + b.containsPlace.length, 0);
-            const ticketTypeGroups = yield ticketTypeRepo.searchTicketTypeGroups({});
-            // 券種グループをランダム選定
-            const ticketTypeGroup = ticketTypeGroups[Math.floor(Math.random() * ticketTypeGroups.length)];
-            const boxOfficeType = yield boxOfficeTypeRepo.findById({ id: ticketTypeGroup.boxOfficeType.id });
-            const duration = Math.floor((Math.random() * 90) + 90);
-            const delay = Math.floor(Math.random() * 780);
-            const doorTime = moment(`${moment().add(Math.floor(Math.random() * 7), 'days').format('YYYY-MM-DD')}T09:00:00+09:00`)
-                .add(delay, 'minutes').toDate();
-            const startDate = moment(doorTime).add(10, 'minutes').toDate();
-            const endDate = moment(startDate).add(duration, 'minutes').toDate();
-            const offers = {
-                typeOf: 'Offer',
-                priceCurrency: chevre.factory.priceCurrency.JPY,
-                availabilityEnds: endDate,
-                availabilityStarts: moment(startDate).add(-7, 'days').toDate(),
-                validFrom: moment(startDate).add(-3, 'days').toDate(),
-                validThrough: endDate,
-                eligibleQuantity: {
-                    value: 4,
-                    unitCode: chevre.factory.unitCode.C62,
-                    typeOf: 'QuantitativeValue'
-                },
-                category: {
-                    id: ticketTypeGroup.id,
-                    name: ticketTypeGroup.name
-                },
-                itemOffered: {
-                    serviceType: {
-                        typeOf: 'ServiceType',
-                        id: boxOfficeType.id,
-                        name: boxOfficeType.name
-                    }
+    const job = new cron_1.CronJob('0 * * * *', () => __awaiter(this, void 0, void 0, function* () {
+        const eventRepo = new chevre.repository.Event(connection);
+        const placeRepo = new chevre.repository.Place(connection);
+        const ticketTypeRepo = new chevre.repository.TicketType(connection);
+        const boxOfficeTypeRepo = new chevre.repository.BoxOfficeType(connection);
+        const eventSeriesList = yield eventRepo.searchScreeningEventSeries({});
+        // イベントシリーズをランダム選定
+        const eventSeries = eventSeriesList[Math.floor(Math.random() * eventSeriesList.length)];
+        // 上映ルームをランダム選定
+        const movieTheater = yield placeRepo.findMovieTheaterByBranchCode(eventSeries.location.branchCode);
+        const screeningRooms = movieTheater.containsPlace;
+        const screeningRoom = screeningRooms[Math.floor(Math.random() * screeningRooms.length)];
+        const maximumAttendeeCapacity = screeningRoom.containsPlace.reduce((a, b) => a + b.containsPlace.length, 0);
+        const ticketTypeGroups = yield ticketTypeRepo.searchTicketTypeGroups({});
+        // 券種グループをランダム選定
+        const ticketTypeGroup = ticketTypeGroups[Math.floor(Math.random() * ticketTypeGroups.length)];
+        const boxOfficeType = yield boxOfficeTypeRepo.findById({ id: ticketTypeGroup.boxOfficeType.id });
+        const duration = Math.floor((Math.random() * 90) + 90);
+        const delay = Math.floor(Math.random() * 780);
+        const doorTime = moment(`${moment().add(Math.floor(Math.random() * 7), 'days').format('YYYY-MM-DD')}T09:00:00+09:00`)
+            .add(delay, 'minutes').toDate();
+        const startDate = moment(doorTime).add(10, 'minutes').toDate();
+        const endDate = moment(startDate).add(duration, 'minutes').toDate();
+        const offers = {
+            typeOf: 'Offer',
+            priceCurrency: chevre.factory.priceCurrency.JPY,
+            availabilityEnds: endDate,
+            availabilityStarts: moment(startDate).add(-7, 'days').toDate(),
+            validFrom: moment(startDate).add(-3, 'days').toDate(),
+            validThrough: endDate,
+            eligibleQuantity: {
+                value: 4,
+                unitCode: chevre.factory.unitCode.C62,
+                typeOf: 'QuantitativeValue'
+            },
+            category: {
+                id: ticketTypeGroup.id,
+                name: ticketTypeGroup.name
+            },
+            itemOffered: {
+                serviceType: {
+                    typeOf: 'ServiceType',
+                    id: boxOfficeType.id,
+                    name: boxOfficeType.name
                 }
-            };
-            const eventAttributes = {
-                typeOf: chevre.factory.eventType.ScreeningEvent,
-                name: eventSeries.name,
-                duration: moment.duration(duration, 'minutes').toISOString(),
-                doorTime: doorTime,
-                startDate: startDate,
-                endDate: endDate,
-                eventStatus: chevre.factory.eventStatusType.EventScheduled,
-                location: {
-                    typeOf: screeningRoom.typeOf,
-                    branchCode: screeningRoom.branchCode,
-                    name: screeningRoom.name,
-                    alternateName: screeningRoom.alternateName,
-                    address: screeningRoom.address,
-                    description: screeningRoom.description
-                },
-                workPerformed: eventSeries.workPerformed,
-                superEvent: eventSeries,
-                offers: offers,
-                maximumAttendeeCapacity: maximumAttendeeCapacity,
-                remainingAttendeeCapacity: maximumAttendeeCapacity,
-                checkInCount: 0,
-                attendeeCount: 0
-            };
-            yield eventRepo.saveScreeningEvent({ attributes: eventAttributes });
-        }), undefined, true);
-        debug('job started', job);
-    }
+            }
+        };
+        const eventAttributes = {
+            typeOf: chevre.factory.eventType.ScreeningEvent,
+            name: eventSeries.name,
+            duration: moment.duration(duration, 'minutes').toISOString(),
+            doorTime: doorTime,
+            startDate: startDate,
+            endDate: endDate,
+            eventStatus: chevre.factory.eventStatusType.EventScheduled,
+            location: {
+                typeOf: screeningRoom.typeOf,
+                branchCode: screeningRoom.branchCode,
+                name: screeningRoom.name,
+                alternateName: screeningRoom.alternateName,
+                address: screeningRoom.address,
+                description: screeningRoom.description
+            },
+            workPerformed: eventSeries.workPerformed,
+            superEvent: eventSeries,
+            offers: offers,
+            maximumAttendeeCapacity: maximumAttendeeCapacity,
+            remainingAttendeeCapacity: maximumAttendeeCapacity,
+            checkInCount: 0,
+            attendeeCount: 0
+        };
+        yield eventRepo.saveScreeningEvent({ attributes: eventAttributes });
+    }), undefined, true);
+    debug('job started', job);
 });
